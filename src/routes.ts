61ac337895e7661514d2cebc8594acc04a3e7163
Nathan Firmo <nathanssfirmo@gmail.com>
2021-06-28 19:01:11 -0300

    Lesson 4

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,18 @@ import { Router } from "express"
 import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateTagController } from "./controllers/CreateTagController"
 import { ensureAdmin } from "./middlewares/ensureAdmin"
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+import { CreateComplimentController } from "./controllers/CreateComplimentsController"
 
 const router = Router()
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
+const authenticateUserController = new AuthenticateUserController()
+const createComplimentController = new CreateComplimentController()
 
 router.post("/users", ensureAdmin, createUserController.handle)
 router.post("/tags", createTagController.handle)
+router.post("/login", authenticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
+
 export { router }
